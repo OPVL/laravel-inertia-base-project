a3124a5ff5936cb54a945bcf919b88d9f34c7f54
Lloyd Culpepper <lloydculpepper4@gmail.com>
2020-12-06 12:22:29 +0000

    react deps updated app.js >> app.jsx default view updated

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <title>
+      @hasSection('title')
+        @yield('title') - {{ config('app.name') }}
+      @else
+        {{ config('app.name') }}
+      @endif
+    </title>
+    <base href="{{ config('app.url') }}">
     <meta charset="utf-8" />
+    {{-- <link href="{{ mix('/css/app.css') }}" rel="stylesheet" /> --}}
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-    <script src="{{ mix('/js/app.js') }}" defer></script>
+    <meta content="{{ csrf_token() }}" name="csrf-token">
+    <script src="{{ asset('/js/app.js') }}" defer></script>
+    @yield('head')
   </head>
   <body>
     @inertia
