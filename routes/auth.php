5c7f157cb0524b1c083e9d05758c108b1b9a6f79
Lloyd Culpepper <lloydculpepper4@gmail.com>
2020-12-08 21:49:37 +0000

    auth routes updated

--- routes/auth.php
+++ routes/auth.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-    Route::get('register')->uses('RegistrationController@create')->name('auth.register');
+    Route::get('register')->uses('RegistrationController@index')->name('auth.register');
     Route::post('register')->uses('RegistrationController@store')->name('auth.register.store');
-    Route::get('login')->name('auth.login');
-    Route::post('login')->name('auth.login.store');
+    Route::get('login')->uses('LoginController@index')->name('auth.login');
+    Route::post('login')->uses('LoginController@store')->name('auth.login.store');
     Route::get('reset-password')->name('auth.reset-password');
     Route::post('reset-password')->name('auth.reset-password.store');
 
